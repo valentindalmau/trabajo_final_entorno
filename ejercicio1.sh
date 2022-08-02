1408013dcf0b38c19e9565ef3f2dbe1616285a19
Valentin Dalmau <Valentindalmau1@gmail.com>
2022-08-02 19:46:51 -0300

    primer ejercicio, statsWords

---
+++ ejercicio1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cont=0
+maxpal=""
+minpal=""
+largomax=0
+largomin=0
+sum=0
+
+[ $# -eq 0 ] && echo "No hay argumentos" && exit 1
+
+for palabra in $@; do
+	largo=`echo $palabra | awk '{print length}'`
+	if [ $cont -eq 0 ]; then
+		maxpal=($palabra)
+		largomax=($largo)
+	fi
+
+	if [ $largo -gt $largomax ]; then
+		maxpal=($palabra)
+		largomax=($largo)
+	fi
+
+	if [ $cont -eq 0 ]; then
+                minpal=($palabra)
+		largomin=($largo)
+	fi
+        if [ $largo -lt $largomin ]; then
+		minpal=($palabra)
+		largomin=($largo)
+	fi
+	sum=$((sum+largo))
+	cont=$((cont+1))
+	done
+promedio=$((sum/cont))
+echo "La palabra mas larga es $maxpal, la mas corta $minpal y el promedio es $promedio"
+exit 0

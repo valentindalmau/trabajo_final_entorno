3e52f110bbcbd873c58ba9a65af8a13cfef1c7d1
Valentin Dalmau <Valentindalmau1@gmail.com>
2022-08-03 21:14:53 -0300

    Ejercicio 2 arreglado, contador para que el top sea de 10 palabras siempre

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -5,8 +5,9 @@ declare -A dict
 
 input=`echo "$@" | awk '{print tolower($0)}'`
 echo $input
+corte=9
 for palabra in $input; do
-	[[ $palabra =~ [^a-zA-Z] ]] && continue
+	[[ $palabra =~ [^a-zA-Z] ]] && corte=$((corte + 1)) && continue
 	largo=`echo $palabra | awk '{print length}'`
 	[ $largo -lt 4 ] && continue
 	dict[$palabra]=$((${dict[$palabra]} + 1))
@@ -16,6 +17,6 @@ echo El top 10 de palabras mas usadas es:
 
 for palabra in ${!dict[@]}; do
        echo $palabra ' - ' ${dict[$palabra]}
-done | sort -rn -k3 | head -n 10
+done | sort -rn -k3 | head -n $corte
 
 exit 0
